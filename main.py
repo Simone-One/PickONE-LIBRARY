80e7e2a0e7b79120f0582de33bf689c54ead83b3
Sio <simone.carassale@gmail.com>
2025-12-24 18:04:16 +0100

    Tasto indietro aggiunto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, session, render_template, request
 from datetime import date
 import smtplib
 from email.message import EmailMessage
@@ -7,6 +7,7 @@ import os
 anno_corrente = str(date.today().year)
 
 app = Flask(__name__)
+app.secret_key = 'c02d69ba291fe90819bbf5b95ecd02ee1e4c7a8a17fd391b69fcdffae3c5e642'
 
 libri = {"Simone Carassale": {"La Grammatica del Graphic Design": ["74"], "Steve Jobs: una biografia illustrata": ["94"]}}
 NOMI_VALIDI = ["Simone Carassale", "Pietro Mergoni", "Giulia Romano", "Lorenzo Giacché", "Andrea Quercioli", "Alessia Amico", "Simone Metteotti"]
@@ -27,6 +28,7 @@ def manda_mail(destinatario, oggetto, testo):
 
 @app.route('/')
 def home():
+    session.clear()
     return render_template('home.html', anno_corrente=anno_corrente)
 
 @app.route('/home', methods=['POST'])
@@ -35,10 +37,16 @@ def verify_user():
     #PROTOCOLLO DI VERIFICA UTENTE; passaggio alla pagina sucessiva
     if name in NOMI_VALIDI:
         #ACCESSO CONSENTITO
+        session['name'] = name
         return render_template('index.html', name=name, libri=libri, anno_corrente=anno_corrente)
     else:
         #ACCESSO NON CONSENTITO; ritorno home con errore
         return render_template('home.html', errore="Nome non valido. Seleziona un nome dalla lista.", anno_corrente=anno_corrente)
+    
+@app.route('/home2')
+def verify_user2():
+    name = session.get('name')
+    return render_template('index.html', name=name, libri=libri, anno_corrente=anno_corrente)
 
 @app.route('/infodati', methods=['POST'])
 def dati():
@@ -60,6 +68,7 @@ def dati():
 
 @app.route('/info')
 def info_oneai():
-    return render_template('info.html', anno_corrente=anno_corrente)
+    name = session.get('name')
+    return render_template('info.html', anno_corrente=anno_corrente, name=name)
 
 app.run(debug=True)
